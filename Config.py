8ecd17221a4f33094e9bfc2fe15fccbd5d68b7f4
roflanGroup <thrillpillwhausen@gmail.com>
2024-10-19 17:35:34 +0500

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,10 @@
+TOKEN = '7718016264:AAGOA_XptpFlPxt-MFEChrzXcGrBdBdgW-M'
+keys = {
+    'Биткоин': 'BTC',
+    'Эфириум': 'ETH',
+    'Доллар': 'USD',
+    'Догикоин': 'DOGE',
+    'Солана': 'SOL',
+    'Евро': 'EUR',
+    'Рубль': 'RUB',
+}
